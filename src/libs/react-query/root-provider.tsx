b6bf0f15b7c14391e1717bcde1b1cf3843855d6b
Hariom Sharma <hariom.s2510@gmail.com>
2025-12-15 02:00:46 +0400

    feat(react-query): add PostHogProvider to root provider
    - integrate PostHog for analytics tracking
    - configure PostHog with environment variables
    - update dependencies to include `posthog-js`

--- src/libs/react-query/root-provider.tsx
+++ src/libs/react-query/root-provider.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
+import { PostHogProvider } from 'posthog-js/react'
 
 export function getContext() {
   const queryClient = new QueryClient()
@@ -16,6 +17,16 @@ export function Provider({
   queryClient: QueryClient
 }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === 'development',
+      }}
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </PostHogProvider>
   )
 }
